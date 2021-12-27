eaf3c41af067411538241ba2b11f849888e6b428
DESKTOP-4QLKFC9\Sean <hsnu510514@gmail.com>
2021-12-27 13:13:39 +0800

    add waterdrop

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -69,6 +68,3 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
-=======
-# css-playground
->>>>>>> a4a84d85f24cbb6ec4bfc9e0c5ce6c9b3b62885c
--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./page/Home";
+import Collection from "./page/Collection";
+import Detail from "./page/Detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="effects" element={<Collection page="effects" />}></Route>
+        <Route path="effects/:effectName" element={<Detail />}></Route>
+        <Route path="components" element={<Collection page="components" />} />
+        <Route path="components/:componentName" element={<Detail />}></Route>
+      </Routes>
+    </Layout>
   );
 }
 
